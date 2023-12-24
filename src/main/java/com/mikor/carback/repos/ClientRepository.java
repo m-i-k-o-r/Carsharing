da11fb726871f85766993c6f4e883f6ca8f25645
m-i-k-o-r <koroli.m.v@gmai.com>
2023-12-25 01:52:36 +0300

    add function findByPhoneNumber

--- src/main/java/com/mikor/carback/repos/ClientRepository.java
+++ src/main/java/com/mikor/carback/repos/ClientRepository.java
@@ -4,7 +4,8 @@ import com.mikor.carback.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
-
+    Optional<Client> findByPhoneNumber(Long PhoneNumber);
 }
